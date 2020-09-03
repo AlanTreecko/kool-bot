a1aeca62eb57e843af726414e20a872385693a94
Alan Treecko <OtherTreecko@gmail.com>
2020-09-03 09:26:12 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ client = discord.Client()
 @bot.event
 async def on_ready():
     print(bot.user.name)
-    await bot.change_presence(activity=discord.Game(name='+help'))
+    await bot.change_presence(activity=discord.Game(name='JESUS IS MY CO-PILOT AND WERE CRUSING FOR PUSSY!'))
     
 # bot commands
 
@@ -34,7 +34,7 @@ async def restart(ctx):
     
 @bot.command()
 async def schedule(ctx):
-    """display the schedule"""
+    """display the morton schedule"""
     channel=ctx.message.channel
     await ctx.send("Here is the schedule.")
     await channel.send(file=discord.File('images/schedule.png'))
@@ -49,73 +49,92 @@ async def watching(ctx, *, arg):
     """change the bot's watching"""
     activity = discord.Activity(name=arg, type=discord.ActivityType.watching)
     await bot.change_presence(activity=activity)
+
+@bot.command(pass_context=True)
+async def dummy(ctx, *user : discord.Member):
+    """DUMMY"""
+    print("DUMMY TAG")
+    print(user)
+    print("FAILSAFETEST CALLED")
+    img1 = await grabimage(ctx, * user)
+    if img1 == None:
+        await ctx.send("I'm sorry, I'm unable to find anything.")
+    else:
+        await ctx.send(img1)
+
+async def grabimage(ctx, *user : discord.Member):
+    print("FAILSAFETEST ACTIVATED")
+    if len(ctx.message.attachments) == 1: # if there is atleast 1 attachment, then do this block
+        attachment = ctx.message.attachments[0] # shorten name
+        attachedurl = attachment.url # copy attachment URL to a smaller name
+        response = requests.get(attachedurl) # get attachment url
+        img1 = Image.open(BytesIO(response.content)).convert('RGBA') # open attachment from URL and convert to a RGBA image
+        return img1 # return img1
+    if len(ctx.message.mentions) == 1:
+        mesmen = ctx.message.mentions # return list of who was mentioned in message
+        user = mesmen[0] # pick the first user in the list
+        userAvatarUrl = user.avatar_url # copy avatar URL to a smaller name
+        response = requests.get(userAvatarUrl) # get avatar URL
+        img1 = Image.open(BytesIO(response.content)).convert('RGBA') # open avatar from URL and convert to a RGBA image
+        return img1
+    else:
+        user = ctx.message.author
+        await ctx.send("Failsafetest result: Mention found. End.") # remove when grabimage is complete
+        userAvatarUrl = user.avatar_url # copy avatar URL to a smaller name
+        response = requests.get(userAvatarUrl) # get avatar URL
+        img1 = Image.open(BytesIO(response.content)).convert('RGBA') # open avatar from URL and convert to a RGBA image
+        return img1 # return img1
     
 @bot.command(pass_context=True)
 async def rotate(ctx, *,  avamember : discord.Member=None):
-    """rotate AVATARS"""
-    userAvatarUrl = avamember.avatar_url # copy avatar URL to a smaller name
-    response = requests.get(userAvatarUrl)
-    img = Image.open(BytesIO(response.content)).convert('RGBA') # open image from URL and convert to a transparent RGB image
-    rotated = img.rotate(random.randint(1, 360), expand=True) # rotate the image with a random range of 1 to 360 degrees
-    rotated.save('rotated.png') # saves the rotated image
+    """rotates images by a random amount"""
+    img1 = await grabimage(ctx)
+    rotated = img1.rotate(random.randint(1, 360), expand=True) # rotate the image with a random range of 1 to 360 degrees
+    rotated.save('tmp/rotate.png') # saves the rotated image
     channel=ctx.message.channel
-    await channel.send(file=discord.File('rotated.png')) # sends image
-    #BUG: WORKS WITH MENTIONS AND TAGS BUT NOT WITH ID!
-    #BUG: CANNOT DETECT URLS THAT ARENT AVATARS!
-    
-    
-    # Test Command for Failsafe
-    
-@bot.command()
-async def failsafetest(ctx):
-    """failsafe test"""
-    msgatch = ctx.message.attachments[0]
-    
-    if msgatch.url == "":
-        msgatch = avamember.avatar_url
-    response = requests.get(msgatch.url)
-    img2 = Image.open(BytesIO(response.content))
-    result = img2.copy()
-    result.save('imt.png') # saves the rotated image
-    channel=ctx.message.channel
-    await channel.send(file=discord.File('imt.png'))
+    await channel.send(file=discord.File('tmp/rotate.png')) # sends image
     
 @bot.command(pass_context=True)
-async def giw(ctx, *,  avamember : discord.Member=None):
-    """imports your avatar to the god i wish that were me meme"""
-    userAvatarUrl = avamember.avatar_url # copy avatar URL to a smaller name
-    response = requests.get(userAvatarUrl)
-    font = ImageFont.truetype("arial.ttf", 24)
-    img1 = Image.open("common/godiwish-base.png") # open base image
-    img2 = Image.open(BytesIO(response.content)) # open avatar and resize to avatar size on base
-    img2s = img2.resize((106, 106))
-    back_img = img1.copy()
-    back_img.paste(img2s, (9, 16))
+async def giw(ctx, * user : discord.Member):
+    """imports images to the god i wish that were me meme"""
+    img1 = await grabimage(ctx)
+    img2 = Image.open("common/godiwish-base.png").convert('RGBA') # open base image
+    img1s = img1.resize((106, 106))
+    giw = img2.copy()
+    giw.paste(img1s, (9, 16))
     #draw = ImageDraw.Draw(back_img)
     #draw.text((175, 30), "alan", (0, 0, 0), font=font)
-    back_img.save('back_img.png') # saves the rotated image
+    giw.save('tmp/giw.png') # saves the rotated image
     channel=ctx.message.channel
-    await channel.send(file=discord.File('back_img.png')) # sends image
-    #BUG: WORKS WITH MENTIONS AND TAGS BUT NOT WITH ID!
-    #BUG: CANNOT DETECT URLS THAT ARENT AVATARS!
+    await channel.send(file=discord.File('tmp/giw.png')) # sends image
     
 @bot.command(pass_context=True)
 async def imt(ctx, *,  avamember : discord.Member=None):
-    """imports anthony onto a picture"""
+    """imports anthony onto an image"""
+    img1 = await grabimage(ctx)
+    img2 = Image.open("common/imadethis-base.png") # open base image
+    w, h = img2.size
+    img1s = img1.resize((494, 461), resample=2)
+    result = Image.new('RGBA', (494,461), (0, 0, 0, 0,))
+    result.paste(img1s, (0,0))
+    result.paste(img2, (0,0), mask=img2)
+    result.save('tmp/imt.png') # saves the rotated image
+    channel=ctx.message.channel
+    await channel.send(file=discord.File('tmp/imt.png')) # sends image
+
+@bot.command(pass_context=True)
+async def aimd(ctx, *,  avamember : discord.Member=None):
+    """ INCOMPLETE """
     userAvatarUrl = avamember.avatar_url # copy avatar URL to a smaller name
     response = requests.get(userAvatarUrl)
-    img1 = Image.open("common/imadethis-base.png") # open base image
-    img2 = Image.open(BytesIO(response.content))#.convert('RGBA') # open avatar and resize to avatar size on base
-    w, h = img1.size
-    img2s = img2.resize((494, 461), resample=2)
-    result = Image.new('RGBA', (494,461), (0, 0, 0, 0,))
-    result.paste(img2s, (0,0))
-    result.paste(img1, (0,0), mask=img1)
-    result.save('imt.png') # saves the rotated image
+    img = Image.open(BytesIO(response.content))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("common/comicsans.ttf", 24)
+    w, h = img.size
+    draw.text((w / 2 , h / 2), "made by anthony ibarra", (0, 0, 0), font=font)
+    img.save('aimd.png') # saves the rotated image
     channel=ctx.message.channel
-    await channel.send(file=discord.File('imt.png')) # sends image
-    #BUG: WORKS WITH MENTIONS AND TAGS BUT NOT WITH ID!
-    #BUG: CANNOT DETECT URLS THAT ARENT AVATARS!
+    await channel.send(file=discord.File('aimd.png')) # sends image
 
 @bot.command()
 async def oso_chistoso(ctx):
